0590126e5cb7e8ea7df74767ecbece62e9554281
Nick Mars <remember.marsman@gmail.com>
2023-03-21 22:02:26 -0400

    change implementation to a class

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,22 +1,50 @@
-export function toUpperCase(arg: string){
-    return arg.toUpperCase();
-}
 
 export type stringInfo = {
-     lowerCase: string,
-     upperCase: string,
-     characters: string [],
-     length: number,
-     extraInfo: Object | undefined
+    lowerCase: string,
+    upperCase: string,
+    characters: string [],
+    length: number,
+    extraInfo: Object | undefined
 }
 
-
-export function getStringInfo(arg: string): stringInfo{
-    return {
-        lowerCase: arg.toLowerCase(),
-        upperCase: arg.toUpperCase(),
-        characters: Array.from(arg),
-        length: arg.length,
-        extraInfo: {message: `your parameter was ${arg}`}
+class StringUtils {
+    toUpperCase(arg: string){
+        if(!arg){
+            throw new Error("Invalid arguments");
+        }
+        return arg.toUpperCase();
+    }
+    getStringInfo(arg: string): stringInfo{
+        return {
+            lowerCase: arg.toLowerCase(),
+            upperCase: arg.toUpperCase(),
+            characters: Array.from(arg),
+            length: arg.length,
+            extraInfo: {message: `your parameter was ${arg}`}
+        }
     }
 }
+
+export default StringUtils
+// export function toUpperCase(arg: string){
+//     return arg.toUpperCase();
+// }
+
+// export type stringInfo = {
+//      lowerCase: string,
+//      upperCase: string,
+//      characters: string [],
+//      length: number,
+//      extraInfo: Object | undefined
+// }
+
+
+// export function getStringInfo(arg: string): stringInfo{
+//     return {
+//         lowerCase: arg.toLowerCase(),
+//         upperCase: arg.toUpperCase(),
+//         characters: Array.from(arg),
+//         length: arg.length,
+//         extraInfo: {message: `your parameter was ${arg}`}
+//     }
+// }
--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,13 +1,56 @@
-import { getStringInfo, stringInfo, toUpperCase } from "../app/utils";
+import StringUtils, {stringInfo} from "../app/utils";
 
 const mockData: string = 'abc';
 const expected: string = 'ABC';
+let stringUtils: StringUtils;
 describe('Utils test', ()=>{
+    //JEST Hooks
+    beforeAll(()=>{
+        // set up
+    })
+    beforeEach(()=>{
+        stringUtils = new StringUtils();
+    })
+    afterEach(()=>{
+        // clearing mocks
+    })
+    afterAll(()=>{
+        // clearing set up
+    })
     it('should return uppercase of valid string', ()=>{
-        const fn: Function = toUpperCase;
+        const fn: Function = stringUtils.toUpperCase;
         const result: string = fn(mockData);
         expect(result).toBe(expected);
     })
+    describe('ToUpperCase test for errors example', () =>{
+        it('should throw error on invalid argument - function', ()=>{
+            const fn: Function = stringUtils.toUpperCase;
+            function expectError(){
+                const actual = fn('');
+            }
+            expect(expectError).toThrow();
+            expect(expectError).toThrowError('Invalid arguments');
+        })
+    
+        it('should throw error on invalid argument - arrow function', ()=>{
+            const fn: Function = stringUtils.toUpperCase;
+            expect( ()=>{fn('')} ).toThrow();
+            expect( ()=>{fn('')} ).toThrowError('Invalid arguments');
+        })
+        fit('should throw error on invalid argument - try catch block', (done)=>{
+            try{
+               const fn: Function = stringUtils.toUpperCase;
+                fn('');
+                //    fail('GetStringInfo should throw error for invalid arg!')
+                done('GetStringInfo should throw error for invalid arg!')
+            }catch(error){
+                expect(error).toBeInstanceOf(Error);
+                expect(error).toHaveProperty('message', 'Invalid arguments');
+                done();
+            }
+        })
+    })
+
     // write more functionality with less code
     describe('ToUpperCase examples', ()=>{
         it.each([
@@ -15,7 +58,7 @@ describe('Utils test', ()=>{
             { input: 'My-Name', expected: 'MY-NAME'},
             { input: 'nick', expected: 'NICK'},
         ])('$input toUpperCase should be $expected', ({input, expected})=>{
-            const actual :string = toUpperCase(input);
+            const actual :string = stringUtils.toUpperCase(input);
             expect(actual).toBe(expected); 
         })
     })
@@ -23,20 +66,20 @@ describe('Utils test', ()=>{
 
     describe('getStringInfo for arg abc should', ()=>{
         it('return right length', ()=>{
-            const result: stringInfo = getStringInfo(mockData);
+            const result: stringInfo = stringUtils.getStringInfo(mockData);
             expect(result.length).toBe(mockData.length);
         })
         it('return right lower case', ()=>{
-            const result: stringInfo = getStringInfo(mockData);
+            const result: stringInfo = stringUtils.getStringInfo(mockData);
             expect(result.lowerCase).toBe(expected.toLowerCase());
         })
         it('return right upper case', ()=>{
-            const result: stringInfo = getStringInfo(mockData);
+            const result: stringInfo = stringUtils.getStringInfo(mockData);
             expect(result.upperCase).toBe(expected.toUpperCase());
         })
 
         it('return right characters', ()=>{
-            const result: stringInfo = getStringInfo(mockData);
+            const result: stringInfo = stringUtils.getStringInfo(mockData);
             expect(result.characters).toEqual(Array.from(mockData));
             expect(result.characters).toHaveLength(mockData.length);
             expect(result.characters).toContain<string>('b');
@@ -44,7 +87,7 @@ describe('Utils test', ()=>{
             expect(result.characters).toEqual(expect.arrayContaining(['b','a']));
         })
         it('return defined extra info', ()=>{
-            const result: stringInfo = getStringInfo(mockData);
+            const result: stringInfo = stringUtils.getStringInfo(mockData);
             expect(result.extraInfo).not.toBe(undefined);
             expect(result.extraInfo).not.toBeUndefined();
             expect(result.extraInfo).toBeDefined();
